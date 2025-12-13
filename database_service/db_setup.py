efdadfe030c9b4e50f00dee263d7315134d16231
dhdschool <dhdschool@outlook.com>
2025-12-13 14:56:03 -0600

    Add get db session function in db_setup.py

--- database_service/db_setup.py
+++ database_service/db_setup.py
@@ -1,3 +1,5 @@
+from typing import AsyncGenerator
+
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
@@ -14,3 +16,21 @@ engine = create_async_engine(Settings().DB_SERVICE_URL, echo=False)
 AsyncSessionLocal = async_sessionmaker(
     bind=engine, class_=AsyncSession, expire_on_commit=False
 )
+
+
+async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
+    """Creates a database connection that rollsback on errors
+
+    Yields:
+        Iterator[AsyncGenerator[AsyncSession, None]]: An async session instance
+    """
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+
+        except Exception:
+            await session.rollback()
+            raise
+
+        finally:
+            await session.close()
