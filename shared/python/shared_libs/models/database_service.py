5068fea72b8d1465b41062400136200116f498b2
dhdschool <dhdschool@outlook.com>
2025-12-13 15:08:51 -0600

    Add etag and poll frequency to shared FeedDTO, add endpoints in database_service main.py

--- shared/python/shared_libs/models/database_service.py
+++ shared/python/shared_libs/models/database_service.py
@@ -11,5 +11,7 @@ class FeedDTO(BaseModel):
     title: Optional[str]
     is_active: bool
     last_fetched_at: Optional[datetime]
+    etag: Optional[str]
+    poll_frequency: int
 
     model_config = ConfigDict(from_attributes=True)
