abc7c17ec625c0b73043b008d858230149e00bdd
dhdschool <dhdschool@outlook.com>
2025-12-13 15:46:03 -0600

    Change config.py to read from local variables if env doesn't exist, add dummy env var in github actions for tests

--- database_service/config.py
+++ database_service/config.py
@@ -3,10 +3,15 @@ from pathlib import Path
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 BASE_DIR = Path(__file__).resolve().parent
+ENV_FILE_PATH = BASE_DIR / ".env"
 
 
 class Settings(BaseSettings):
     DB_SERVICE_URL: str
     LOG_LEVEL: str = "INFO"
 
-    model_config = SettingsConfigDict(env_prefix="MP_", env_file=BASE_DIR / ".env")
+    model_config = SettingsConfigDict(
+        env_prefix="MP_",
+        env_file=ENV_FILE_PATH if ENV_FILE_PATH.exists() else None,
+        extra="ignore",
+    )
